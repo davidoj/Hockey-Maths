28de0f200c29470498a99f230450dc63c15f3ccf
davidoj <orion.tjungarryi@gmail.com>
2015-06-24 15:55:21 +1000

    Pre-branching commit

--- maths.lua
+++ maths.lua
@@ -13,7 +13,7 @@ Question_mt = { __index = Question }
 
 Params = {
    range = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
-   ops = {'+','-'} -- 'x'}--,'/'}
+   ops = {'+','-'} 
 }
 
 function Question:execute()
